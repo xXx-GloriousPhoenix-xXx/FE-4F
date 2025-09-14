12b4176c09dede3b8cd4a5c249f8f009cf9cb6c8
RomanPR <prikhodjko.roman@gmail.com>
2025-09-14 03:24:54 +0300

    lab7 init

---
+++ lab7/src/components/ImageEditor/ImageEditor.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import MyButton from "../UI/MyButton/MyButton";
+import MyInput from "../UI/MyInput/MyInput";
+import MyImage from "../UI/MyImage/MyImage";
+
+import cl from "./ImageEditor.module.css"
+
+import kharkivImg from "../../assets/img/kharkiv-cut.png"
+
+const ImageEditor = () => {
+    const [deltaSize, setDeltaSize] = useState(0);
+    const [inputValue, setInputValue] = useState("");
+    const [imageRef, setImageRef] = useState(kharkivImg);
+    const handleResize = (change) => {
+        setDeltaSize(deltaSize + change);
+    }
+    const handleRefChange = () => {
+        setImageRef(inputValue);
+    }
+    const handleClear = () => {
+        setImageRef("");
+    }
+    return ( 
+        <div className={cl.ImageEditor}>
+            <div className={cl.ControlWrapper}>
+                <MyButton text={"Додати"} onClick={() => handleRefChange()}/>
+                <MyButton text={"Збільшити"} onClick={() => handleResize(50)}/>
+                <MyButton text={"Зменшити"} onClick={() => handleResize(-50)}/>
+                <MyButton text={"Видалити"} onClick={() => handleClear()}/>
+                <MyInput
+                    placeholder={"Посилання..."}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+            </div>
+            <MyImage startSize={400} deltaSize={deltaSize} path={imageRef}/>
+        </div>
+    );
+}
+ 
+export default ImageEditor;
