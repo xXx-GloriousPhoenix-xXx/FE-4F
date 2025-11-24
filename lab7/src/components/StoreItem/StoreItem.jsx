72bc5a84a3cf5bbbaaa9e7ea563eb67a665ece79
RomanPR <prikhodjko.roman@gmail.com>
2025-11-24 09:48:28 +0200

    lab7 fix

--- lab7/src/components/StoreItem/StoreItem.jsx
+++ lab7/src/components/StoreItem/StoreItem.jsx
@@ -5,9 +5,11 @@ const StoreItem = ({content}) => {
         <div className={cl.Wrapper}>
             <div className={cl.Image}>
                 <img src={content.ref} alt='store-item' />
+                <p>{content.price}</p>
             </div>
-            <h3>{content.name}</h3>
-            <p>{content.price}</p>
+            <div className={cl.Description}>
+                <h3>{content.name}</h3>
+            </div>
             <button className={cl.Button}>Додати до кошику</button>
         </div>
     );
